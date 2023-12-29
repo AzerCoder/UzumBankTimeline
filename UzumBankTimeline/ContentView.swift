7fa6af2f00f8109087ed314147e5af35bdcffb25
A'zamjon Abdumuxtorov <azamjonabdumuxtorov1220@gmail.com>
2023-12-29 15:17:02 +0500

    Initial Commit

---
+++ UzumBankTimeline/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  UzumBankTimeline
+//
+//  Created by A'zamjon Abdumuxtorov on 29/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ UzumBankTimeline/UzumBankTimelineApp.swift
@@ -0,0 +1,17 @@
+//
+//  UzumBankTimelineApp.swift
+//  UzumBankTimeline
+//
+//  Created by A'zamjon Abdumuxtorov on 29/12/23.
+//
+
+import SwiftUI
+
+@main
+struct UzumBankTimelineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
